a262793c966fc3976dcf2f9da2de80263030401a
Tarasov Sergei <www.neon@mail.ru>
2022-12-19 18:57:19 +0500

    homework 4-3 в процессе выполнения

---
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -0,0 +1,12 @@
+-- liquibase formatted sql
+
+
+
+-- 1) Индекс для поиска по имени студента.
+-- changeset starasov:1
+
+
+
+
+-- 2) Индекс для поиска по названию и цвету факультета.
+-- changeset starasov:2
